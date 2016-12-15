70254ca982bae21d3a5fb6129bf3c112d591921c
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-12-15 11:25:00 -0500

    Layout files

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,3 +1,5 @@
+@extends('layout.mainLayout')
+@section('content')
 <h1> About Page </h1>
 <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
@@ -10,11 +12,14 @@ foreach($people as $person){
 ?>
 
 <h3> blade foreach </h3>
+<p>Dogs:</p>
+  @if (empty($dog))
+  <p> There are no Dogs</p>
+  @endif
 
-@if (empty($dog))
-<p> There are no Dogs</p>
-@endif
+<p>People:</p>
+  @foreach($people as $person)
+    {{$person}} <br>
+  @endforeach
 
-@foreach($people as $person)
-  {{$person}} <br>
-@endforeach
+@stop
