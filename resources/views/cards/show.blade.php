c6d1d39454d25dcf76d2ac1c76514c2b3e0b9248
mthomps4 <matthew.thompson.a@gmail.com>
2017-01-04 19:06:01 -0500

    foreach Note view

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -1,4 +1,9 @@
 @extends('layout.mainLayout')
 @section('content')
   <h1> {{ $card->title}}</h1>
+  <p>
+    @foreach($card->notes as $note)
+      Note: {{$note->body}} <br>
+    @endforeach
+  </p>
 @stop
