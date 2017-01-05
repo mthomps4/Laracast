ed38bdddd411120b150235596797ea1ce6a49e56
mthomps4 <matthew.thompson.a@gmail.com>
2017-01-05 16:55:16 -0500

    Add Card controller and form

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 //use DB;DB::table('cards')->get();
+use DB;
 use App\Card; //
 use Illuminate\Http\Request;
 
@@ -18,5 +19,17 @@ class CardsController extends Controller
     {
       $card = Card::find($id);
       return view('cards.show', compact('card'));
+    }
+
+    public function store(Request $request)
+    {
+      $card = new Card();
+      $card->title = $request->title;
+      // $card->created_at = new DateTime;
+      // $card->updated_at = new DateTime;
+
+      $card->save();
+
+      return back();
     }
 }
--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -6,6 +6,23 @@
     <div>
     <a href="<?php echo "cards/" . $card->id ?>">  {{$card->title}}</a>
     </div>
-
   @endforeach
+
+    <hr>
+
+      <h3> Add a New Card </h3>
+
+      <form method="post" action="/cards">
+        <div class="form-group">
+          <label for="title"> Title: </label>
+          <input name="title" class="form-group-item" />
+        </div>
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Add Note</button>
+        </div>
+
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+      </form>
 @stop
